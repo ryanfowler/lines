fba820a65665324a442a9c83a81b4152bece3341
ryanfowler <ryan.fowler19@gmail.com>
2015-05-20 18:05:16 -0400

    Add go sample file

---
+++ counter/test_files/go_sample.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// sample block comment
+	fmt.Println("Print something") // mix line
+	fmt.Println("Something else")  /* block on code - mix line */
+	/*
+	   Block comment
+	   of 4 lines
+	*/
+}
